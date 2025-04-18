4f306d323ea531bd11696a32985de946fd21e0de
Xiangfan Piao <xiangfan.piao@avanade.com>
2025-04-18 16:04:05 +0900

    Mixture model -> E-step complete

--- naive_em.py
+++ naive_em.py
@@ -1,6 +1,7 @@
 """Mixture model using EM"""
 from typing import Tuple
 import numpy as np
+from sklearn.covariance import log_likelihood
 from common import GaussianMixture
 
 
@@ -17,8 +18,40 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+    n, d = X.shape
+    K, _ = mixture.mu.shape
+    post = np.zeros((n, K))
+    log_likelihood = 0
+    # p( i | theta ) = p_1* (1 / sqrt(2*np.pi)*sigma * np.exp(-(x-mu)**2/2*sigma**2))
 
+    for k in range(K):
+
+        # (n, d) - (d, ) -> (n, d)
+        diff = X - mixture.mu[k]
+        # exp部分: -(x - mu)**2 / (2 * sigma**2)
+        # sum: (n, d) -> (n,), mixture.var[k]はスカラー (1, )
+        # (n,) / (1,) -> (n,)
+        exponent = -0.5 * np.sum((diff**2), axis=1) / mixture.var[k]
+
+        # ガウス密度 (係数含む) -> (n, )
+        coef = 1 / (2 * np.pi * mixture.var[k])**(d / 2)
+        gaussian = coef * np.exp(exponent)
+
+        # 重みを掛けて加算 (n, )
+        post[:, k] = mixture.p[k] * gaussian
+
+    # 各データ点の属するクラスの合計 (n, k) -> (n, 1)
+    # 行ごとに合計を取る -> p1*gaussian + p2*gaussian + p3*gaussian + ...
+    post_sum = np.sum(post, axis=1, keepdims=True)
+
+    # posterior soft assignment (n, k)
+    post /= post_sum
+
+    # log-likelihood
+    log_likelihood = np.sum(np.log(post_sum))
+
+    return post, log_likelihood
+    
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     """M-step: Updates the gaussian mixture by maximizing the log-likelihood
